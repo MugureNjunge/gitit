013b758890b5e04a4f192e2e4838c2e3a77cd5f7
MugureNjunge <maureennjunge22@gmail.com>
2022-04-18 18:58:46 +0300

    make the correct declarations

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -3,10 +3,11 @@ import { ProfileService } from '../profile.service';
 
 @Component({
   selector: 'app-profile',
-  templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.scss']
+  templateUrl : './profile.component.html',
+  styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
+
   
   profile = { 
     login: '',
@@ -41,13 +42,13 @@ export class ProfileComponent implements OnInit {
   findProfile() {
     this.service.getProfileInfo().subscribe(profile => {
       console.log(profile);
-      this.profile = profile;
+      this; this.profile 
       
     });
 
     this.service.getProfileRepos().subscribe(repos => {
       console.log(repos);
-     this; this.repos = repos;
+     this; this.repos 
     });
   }
 
